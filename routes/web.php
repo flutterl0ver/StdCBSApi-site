ac86f0bd6bcccda17d7021a3100fabab07a4ed07
fluttershY_ <yurchuk.pasha10@mail.ru>
2025-06-24 11:29:40 +0200

    убрал ненужные руты

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\RequestController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,9 +9,5 @@ Route::view('/flights', 'flights');
 Route::post('/get-token', [SearchController::class, 'getToken']);
 Route::post('/search', [SearchController::class, 'search']);
 Route::post('/get-flight-data', [SearchController::class, 'getFlightData']);
-
-Route::post('/request/searchflights', [RequestController::class, 'searchFlights']);
-Route::post('/request/searchresult', [RequestController::class, 'searchResult']);
-Route::post('/request/flight', [RequestController::class, 'flight']);
 
 Route::get('/search-airports', [SearchController::class, 'searchAirports']);
